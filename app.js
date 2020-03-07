1b7d998c82a09ad7f4fc0e278dfe6ac479ac0534
Roy Reyna <royreyna7@yahoo.com>
2020-03-07 13:59:36 -0600

    functions defined

--- app.js
+++ app.js
@@ -1,34 +1,89 @@
+
 var API_key = "pmFmeC78sVzcqR0XlcIlnt5MTUi82reS";
+var gifRating;
+var motion = "still";
+var harryPotter = ["Harry_Potter", "Hermione_Granger", "Ron_Weasley", "Severus_Snape", "Albus_Dumbledore", "Voldemort", "Hagrid", "Sirius_Black", "Draco_Malfoy", "Neville_Longbottom", "Hedwig"];
 
-var search = "football";
-var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=" + API_key + "&limit=10";
+printButtons();
 
-var harryPotter=["Harry Potter", "Hermione Granger", "Ron Weasley", "Severus Snape", "Albus Dumbledore", ];
-$.ajax({
-  url: queryURL,
-  method: "GET"
-}).then(function (response) {
-  console.log(response.data[0]);
+function printButtons() {
 
-  
+  $("#buttons").empty();
 
-  for (i = 0; i < 10; i++) {
+  for (i = 0; i < harryPotter.length; i++) {
 
-    var gifBin = $("<div id ='gifBin'>");
+    var character = harryPotter[i].replace(/_/g," ");
+    var button = $("<button>");
 
-    var gifRating = $("<span>")
-    var rating = response.data[i].rating;
-    gifRating.html("Rating: " + rating.toUpperCase() + "<br>");
+    button.attr("class", "gif");
+    button.html(character);
+    button.attr("value",character);
 
-    var newGif = $("<img id = 'gif'>");
-    newGif.attr("src", response.data[i].images.downsized_still.url);
+    $("#buttons").append(button);
 
-    $("#gifBin").append(gifRating);
-    $("#gifBin").append(newGif);
-
-    $("#gifs_container").append(gifBin);
+    //console.log(harryPotter[i]);
   }
+}
 
+function printGifs(name) {
 
+  var search = name;
+  var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=" + API_key + "&limit=10";
 
+  console.log(queryURL);
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
+
+    console.log(response);
+
+    for (i = 0; i < 10; i++) {
+
+      var gifBin = $("<div>");
+      gifBin.attr("id", "images");
+
+      var rating = response.data[i].rating;
+      rating = "Rating: " + rating.toUpperCase();
+
+      var newGif = $("<img id = 'gif'>");
+
+      newGif.attr("data-still", response.data[i].images.downsized_still.url);
+      newGif.attr("data-animate", response.data[i].url);
+      newGif.attr("still-state", "still");
+      newGif.attr("src", newGif.attr("data-still"));
+
+      gifBin.html(rating + "<br>");
+      gifBin.append(newGif);
+
+      $("#gifs_container").append(gifBin);
+    }
+
+  });
+
+}
+
+$(document.body).on("click", ".gif", function () {
+
+  //$("#gif_container").empty()
+
+  search = $(this).val();
+  //console.log(search);
+  printGifs(search);
 });
+
+
+$(document.body).on("click", "#images", function () {
+
+    if($(this).attr("still-state") === "still"){
+      $(this).attr("src", "data-animate");
+      $(this).attr("still-state", "animate");
+    }
+
+    if($(this).attr("still-state")=== "animate"){
+      $(this).attr("src", "data-still");
+      $(this).attr("still-state", "still");
+    }
+});
+
